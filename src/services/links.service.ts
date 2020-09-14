f66ea674605158afe92400933dac8d908ce20369
klapeto <yiannispana@hotmail.com>
2020-09-14 12:54:49 +0300

    Added models for Download and links

--- src/services/links.service.ts
+++ src/services/links.service.ts
@@ -1,18 +1,57 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {DownloadInstance} from '../models/download-instance';
+import {Link} from '../models/link';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LinksService {
 
-  public readonly linuxLatestBeta = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/Elpida-latest-x86_64.AppImage?job=build:linux';
-  public readonly linuxLatestBetaChecksum = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/SHA256SUMS?job=build:linux';
-  public readonly windowsLatestBeta = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/Elpida-latest-x86_64.zip?job=build:windows';
-  public readonly windowsLatestBetaChecksum = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/SHA256SUMS?job=build:windows';
-  public readonly latestSource = 'https://gitlab.com/dev-hood/elpida/elpida/-/archive/develop/elpida-develop.tar.gz';
-  public readonly sourceCodeRepo = 'https://gitlab.com/dev-hood/elpida/elpida';
+    public readonly downloadLinks = [
+        new DownloadInstance('Windows',
+            [
+                new Link(
+                    'Beta Zip (x86-64)',
+                    'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/Elpida-latest-x86_64.zip?job=build:windows'
+                )
+            ],
+            [
+                new Link(
+                    'sha256sum',
+                    'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/SHA256SUMS?job=build:windows'
+                )
+            ]
+        ),
+        new DownloadInstance('Linux',
+            [
+                new Link(
+                    'Beta AppImage (x86-64)',
+                    'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/Elpida-latest-x86_64.AppImage?job=build:linux'
+                )
+            ],
+            [
+                new Link(
+                    'sha256sum',
+                    'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/SHA256SUMS?job=build:linux'
+                )
+            ]
+        ),
+        new DownloadInstance('Source code',
+            [
+                new Link(
+                    'Beta Source code',
+                    'https://gitlab.com/dev-hood/elpida/elpida/-/archive/develop/elpida-develop.tar.gz'
+                )
+            ],
+            [
+                new Link(
+                    'Git Repository',
+                    'https://gitlab.com/dev-hood/elpida/elpida'
+                )
+            ]
+        ),
+    ];
+    constructor() {
 
-  constructor() {
-
-  }
+    }
 }
