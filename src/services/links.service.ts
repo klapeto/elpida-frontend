1913300466facdf87a112c79ea04f9b1446b123e
klapeto <yiannispana@hotmail.com>
2020-09-14 12:22:11 +0300

    Modified download page
     - added more information

--- src/services/links.service.ts
+++ src/services/links.service.ts
@@ -6,7 +6,9 @@ import { Injectable } from '@angular/core';
 export class LinksService {
 
   public readonly linuxLatestBeta = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/Elpida-latest-x86_64.AppImage?job=build:linux';
+  public readonly linuxLatestBetaChecksum = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/SHA256SUMS?job=build:linux';
   public readonly windowsLatestBeta = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/Elpida-latest-x86_64.zip?job=build:windows';
+  public readonly windowsLatestBetaChecksum = 'https://gitlab.com/dev-hood/elpida/elpida/-/jobs/artifacts/develop/raw/SHA256SUMS?job=build:windows';
   public readonly latestSource = 'https://gitlab.com/dev-hood/elpida/elpida/-/archive/develop/elpida-develop.tar.gz';
   public readonly sourceCodeRepo = 'https://gitlab.com/dev-hood/elpida/elpida';
 
